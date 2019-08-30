6468c0c316df1e964c57694db9322c6d86da8507
wangzhankun <bitwangzhankun@gmail.com>
2019-08-30 15:27:55 +0800

    最原始配置完整的环境

---
+++ include/config.h
@@ -0,0 +1,17 @@
+/*
+ * @Copyright: CS of BIT
+ * @Author: 王占坤
+ * @File name: 
+ * @Version: 
+ * @Date: 2019-08-30 15:19:46 +0800
+ * @LastEditTime: 2019-08-30 15:20:14 +0800
+ * @LastEditors: 
+ * @Description: 这里主要放置一些配置，主要用于代码生成方便
+ */
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+
+
+
+#endif
