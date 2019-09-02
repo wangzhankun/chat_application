cba08898c2c6f350b377ef361401850358a3904c
何禾子 <jiyi00022010@gmail.com>
2019-09-02 03:37:40 -0700

    添加主界面好友列表

--- src/main.c
+++ src/main.c
@@ -4,11 +4,11 @@
  * @Author: sueRimn
  * @Date: 2019-08-29 23:38:17
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2019-09-01 19:45:52 -0700
+ * @LastEditTime: 2019-09-02 03:29:09 -0700
  */
 #include "head.h"
 #include "GTKCreateLoading.h"
-
+#include "Gtkfriendlist.h"
 int main(int argc, char *argv[])
 {
     GtkWidget *LoadingWindow;
@@ -16,6 +16,12 @@ int main(int argc, char *argv[])
     LoadingWindow = CreateLoading();
     g_signal_connect(G_OBJECT(LoadingWindow), "destroy", G_CALLBACK(gtk_main_quit), NULL);
     gtk_widget_show(LoadingWindow);
+
+    GtkWidget *ListWindow;
+    ListWindow = CreateFriendlist();
+    g_signal_connect(G_OBJECT(ListWindow), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    gtk_widget_show(ListWindow);
+
     gtk_main();
     return FALSE;
 }
