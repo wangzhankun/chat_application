1c65a1ddf6590c5d3bb3f59f42c65f002a864952
wangzhankun <bitwangzhankun@gmail.com>
2019-09-04 11:18:14 +0800

    增加了定义

--- include/config.h
+++ include/config.h
@@ -4,7 +4,7 @@
  * @File name: 
  * @Version: 
  * @Date: 2019-08-30 15:19:46 +0800
- * @LastEditTime: 2019-09-03 10:55:44 +0800
+ * @LastEditTime: 2019-09-04 01:22:10 +0800
  * @LastEditors: 
  * @Description: 这里主要放置一些配置，主要用于代码生成方便
  */
@@ -12,7 +12,9 @@
 #define __CONFIG_H__
 
 #define SERVER_PORT 8888
-#define SERVER_ADDR "0.0.0.0"
+#define SERVER_ADDR "192.168.43.18"
+// #define SERVER_ADDR "0.0.0.0"
 
+#define BUFFER_SIZE 1024
 
 #endif
