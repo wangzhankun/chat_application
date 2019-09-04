812a8ef9acf2cea90d4562b3e7b8af8346008695
wangzhankun <bitwangzhankun@gmail.com>
2019-09-04 11:16:16 +0800

    更改了头文件引用顺序

--- include/head.h
+++ include/head.h
@@ -4,7 +4,7 @@
  * @File name: 
  * @Version: 
  * @Date: 2019-08-30 15:08:30 +0800
- * @LastEditTime: 2019-09-03 11:09:17 +0800
+ * @LastEditTime: 2019-09-03 21:25:09 +0800
  * @LastEditors: 
  * @Description: 这里是主头文件，包含了所有的库文件，全局变量声明，结构体声明等
  */
@@ -15,8 +15,8 @@
 #include <cjson/cJSON.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <gtk/gtk.h>
 #include <pthread.h>
+#include <gtk/gtk.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
